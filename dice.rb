777b5c6df7f4f6547613ee680b7c01db3e6ca991
willd12-pixel <143421896+willd12-pixel@users.noreply.github.com>
2024-01-18 15:37:20 +0000

    2d6 works

--- dice.rb
+++ dice.rb
@@ -1,7 +1,27 @@
 # /dice.rb
 
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
   "Hello World"
+end
+
+get("/zebra") do
+  " hello,everyone "
+end 
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
