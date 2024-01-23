c91b061cc7ee62c5fa87fd60ae231624c4dece16
willd12-pixel <143421896+willd12-pixel@users.noreply.github.com>
2024-01-23 15:37:58 +0000

    works

--- dice.rb
+++ dice.rb
@@ -25,3 +25,24 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+et("/dice/2/10") do
+  first_die = rand(1..10)
+  
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
